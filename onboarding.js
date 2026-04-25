94a212f5e180e88e6253a25266a7fe3611495962
hamza4times <241655835+hamza4times@users.noreply.github.com>
2026-04-25 11:34:28 -0500

    Hero section, bg image, pseudocode

--- onboarding.js
+++ onboarding.js
@@ -1,4 +1,56 @@
 /*
+Weight: 
 
+Height:
+
+Preference: (Drop Down Menu Choose One)
+1. Stamina
+ - Stairmaster
+ - Cycling
+ - Swimming
+ - Elliptical
+ - Running
+2. Flexibility
+ - Calf Stretch
+ - Lunge
+ - Hip Circles
+ - Cat-Cow Stretch
+ - Back Bridges
+3. General Fitness
+ - Staairmaster
+ - Cat-Cow Stretch
+ 
+4. Strength
+ - Bench Press
+ - Squats
+ - Deadlifts
+ - Overhead Press
+ - Weighted Pullups
+5. Volume
+ - Bicep Curls
+  - Tricep Rope Pushdown
+   - Leg Ext
+dn
+
+  
+
+
+Enter Button
+
+Weight:
+Small: <= 110
+Medium: 110 <= x <= 160
+High: 160 >=
+
+Height:
+Short: <= 5 ft
+Medium: 5 <= x <= 5'8
+Tall: 5'8 >
+
+
+Preferences:
+
+
+""
 
 */
